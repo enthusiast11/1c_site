83a1dc87d3dd7e5f6fd1c7898250b15009c09795
Ivan Zhidkov <ivanzidkov60@gmail.com>
2024-12-18 14:20:38 +0300

    Revert commit

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,10 @@
 import { Container, Typography } from "@mui/material";
 import React, { useEffect } from "react";
-import { useGetExcelQuery } from "./store/rtk-query/excel";
 
 const Main = () => {
-  const { data, error, isLoading } = useGetExcelQuery({});
-
   return (
     <Container sx={{ width: "50%" }}>
-      <Typography> {data && JSON.stringify(data, null, 2)}</Typography>
+      <Typography></Typography>
     </Container>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { loginApi } from "./rtk-query/login";
-import { excelApi } from "./rtk-query/excel";
 
 export const store = configureStore({
   reducer: {
     [loginApi.reducerPath]: loginApi.reducer,
-    [excelApi.reducerPath]: excelApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(loginApi.middleware, excelApi.middleware),
+    getDefaultMiddleware().concat(loginApi.middleware),
 });
 
 setupListeners(store.dispatch);
